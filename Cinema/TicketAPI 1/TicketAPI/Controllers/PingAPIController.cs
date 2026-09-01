9d23a6fdbe616ccce65d37854e6e26e257f45be7
themud91 <perezguillermo85@gmail.com>
2026-09-01 15:49:32 -0400

    ajustes billeteria6

--- Cinema/TicketAPI 1/TicketAPI/Controllers/PingAPIController.cs
+++ Cinema/TicketAPI 1/TicketAPI/Controllers/PingAPIController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System.Runtime.InteropServices;
 using TicketAPI.Repositories;
 using TicketAPI.Services;
 
@@ -10,6 +12,17 @@ namespace TicketAPI.Controllers {
         [HttpGet]
         public OkObjectResult Get() {
             return Ok("{\"ping\":\"pong\",\"diag\":\"v3-global-mw\"}");
-        } 
+        }
+
+        // DIAGNOSTIC TEMPORAIRE : liste les filtres MVC globaux et la version du runtime.
+        [HttpGet("diag-info")]
+        public ActionResult DiagInfo([FromServices] IOptions<MvcOptions> mvcOptions) {
+            var filters = mvcOptions.Value.Filters.Select(f => f.ToString()).ToList();
+            return Ok(new {
+                framework = RuntimeInformation.FrameworkDescription,
+                osDescription = RuntimeInformation.OSDescription,
+                filters
+            });
+        }
     }
 }
